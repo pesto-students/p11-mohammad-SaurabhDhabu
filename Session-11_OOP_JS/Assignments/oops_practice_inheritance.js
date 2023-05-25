4aa6a80f371c59374715e871f53d9fb001a0208c
SaurabhDhabu <sndhabu98e@gmail.com>
2023-05-25 23:03:31 +0530

    Interitance, polymorphism, abstraction

---
+++ Session-11_OOP_JS/Assignments/oops_practice_inheritance.js
@@ -0,0 +1,28 @@
+// interitance - extends and use already defined classes
+
+class Vehicle {
+  constructor(make, model, year, color) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+  }
+
+  drive() {
+    console.log(`Driving ${this.make} ${this.model}.`);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, module, year, color, numSeats) {
+    super(make, module, year, color);
+    this.numSeats = numSeats;
+  }
+}
+
+class RideShareCar extends Car {
+  constructor(make, model, year, color, numSeats, isAvailable) {
+    super(make, model, year, color, numSeats);
+    this.isAvailable = isAvailable;
+  }
+}
